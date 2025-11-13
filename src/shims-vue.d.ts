d4b82f94c061d292ad1795a5d49e152f9fef8e50
AliceZhang <huarong290@gmail.com>
2025-11-13 13:54:21 +0800

    chore(config): 为src目录添加别名

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,15 @@
+//src/shims-vue.d.ts
+
+
+declare module '*.vue' {
+    import type { DefineComponent } from 'vue'
+    // 使用 object 和 unknown 替代 {} 和 any
+    const component: DefineComponent<object, object, unknown>
+    export default component
+}
+
+// 如果还是无法识别 @，可以尝试添加以下声明
+declare module '@/*' {
+    const value: unknown
+    export default value
+}
